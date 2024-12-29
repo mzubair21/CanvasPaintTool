6d5e169af97df2b3472e0b9f5378465ba4035987
mzubair21 <mzubair21@outlook.com>
2024-12-29 06:15:39 +0500

    initial: Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {},
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      spacing: {
+        4.5: "18px",
+      },
+      boxShadow: {
+        "3xl":
+          "0 2px 2px rgb(224 230 237 / 46%), 1px 6px 7px rgb(224 230 237 / 46%)",
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-invert-headings": theme("colors.white.dark"),
+            "--tw-prose-invert-links": theme("colors.white.dark"),
+            h1: { fontSize: "40px", marginBottom: "0.5rem", marginTop: 0 },
+            h2: { fontSize: "32px", marginBottom: "0.5rem", marginTop: 0 },
+            h3: { fontSize: "28px", marginBottom: "0.5rem", marginTop: 0 },
+            h4: { fontSize: "24px", marginBottom: "0.5rem", marginTop: 0 },
+            h5: { fontSize: "20px", marginBottom: "0.5rem", marginTop: 0 },
+            h6: { fontSize: "16px", marginBottom: "0.5rem", marginTop: 0 },
+            p: { marginBottom: "0.5rem" },
+            li: { margin: 0 },
+            img: { margin: 0 },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+    require("@tailwindcss/typography"),
+  ],
+}
